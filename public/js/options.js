98b66267dbcb1f8c07a24604123d02e18459f7e9
alionti998 <alionti998>
2018-09-20 18:49:26 -0400

    Added previous close, cleaned up code

--- public/js/options.js
+++ public/js/options.js
@@ -1,22 +1,16 @@
-
 var today = new Date();
 var dd = today.getDate();
-var mm = today.getMonth(); //January is 0!
+var mm = today.getMonth();
 var yyyy = today.getFullYear();
 
 function getChartOptions(range) {
     if (range == "Day") {
 
         var options = {
-            'title': 'Last Updated ' + metaData["3. Last Refreshed"]/* + " " + metaData["5. Time Zone"]*/,
+            'title': 'Last Updated ' + metaData["3. Last Refreshed"],
             legend: 'none',
             hAxis: {
                 title: '',
-                //showTextEvery: 0,
-                //slantedText: true,
-                //slantedTextAngle: 90,
-                //textPosition : 'in',
-                //for intraday only
                 viewWindow: {
                     min: new Date(yyyy, mm, dd, 9, 30),
                     max: new Date(yyyy, mm, dd, 16),
@@ -27,12 +21,8 @@ function getChartOptions(range) {
                 format: 'currency',
             },
             legend: {
-                //position: 'right', textStyle: {color: 'blue', fontSize: 16},
                 position: 'right',
             }
-            //legend:'none',
-
-            //chartArea:{left:20,top:0,width:'50%',height:'50%'},
         };
     } else if (range == "Week") {
         var options = {
